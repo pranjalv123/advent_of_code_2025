4baa80e3db2eda2fcae7321243de5bce8281ea1e
pranjalv123 <pr@nj.al>
2025-12-03 01:02:51 -0500

    code golf 3

--- 3.py
+++ 3.py
@@ -1,26 +1,7 @@
-def optimize_bank(numbers: list[int], size: int):
+def optimize_bank(numbers:list[int], size: int):
     if size == 1:
         return max(numbers)
-    m = max(numbers[:-size+1])
-    i = numbers.index(m)
-    remainder = optimize_bank(numbers[i+1:], size-1)
-    return remainder + m * (10**(size-1))
+    i, m = max(enumerate(numbers[:-size+1]), key=lambda x: x[1])
+    return m * (10**(size-1)) + optimize_bank(numbers[i+1:], size-1)
 
-
-sample_input="""987654321111111
-811111111111119
-234234234234278
-818181911112111"""
-
-from sys import stdin
-
-total = 0
-for bank in stdin:
-    bank = [int(i) for i in bank.strip()]
-    opt = optimize_bank(bank, 12)
-    print(bank)
-    print(opt)
-    print()
-    total += opt
-
-print(total)
+print(sum([optimize_bank([int(i) for i in bank.strip()], 12) for bank in open("3.input").readlines()]))
