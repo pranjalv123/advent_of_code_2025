261eabcd4fac46125ef4464a52d253982ada0b4f
Pranjal Vachaspati <pvachaspati@ramp.com>
2025-12-03 00:25:49 -0500

    Remove unnecessary print statements

--- 2b.py
+++ 2b.py
@@ -1,8 +1,6 @@
 from itertools import batched
 
 def process_range(a:str, b:str, div: int):
-    print()
-    print(a, b, div)
     if len(a) % div != 0:
         a = '1' + '0' * (div * (1 + len(a)//div) - 1)
     if len(b) % div != 0:
@@ -12,7 +10,6 @@ def process_range(a:str, b:str, div: int):
         return 0
 
     chunk_size = len(a)//div
-    print(a, b, chunk_size)
 
     a_parts = [int(''.join(i)) for i in batched(a, chunk_size)]
     b_parts = [int(''.join(i)) for i in batched(b, chunk_size)]
@@ -23,12 +20,9 @@ def process_range(a:str, b:str, div: int):
     rest_a = a_parts[1:]
     rest_b = b_parts[1:]
     
-    print(first_a, rest_a)
-    print(first_b, rest_b)
 
 
     n = first_b - first_a
-    print(n)
 
     a_inclusive = True
     for r in rest_a:
@@ -48,18 +42,15 @@ def process_range(a:str, b:str, div: int):
             b_inclusive = False
             break
 
-    print(a_inclusive, b_inclusive)
 
 
     if n == 0:
         if a_inclusive and b_inclusive:
-            print(1)
             total= int( str(first_a) * div)
             for i in range(1, chunk_size):
                 if len(set(batched(str(total), i))) == 1:
                     print("skipping", total, i)
                     return 0
-            print("!", total)
             return total
         return 0
 
@@ -74,7 +65,6 @@ def process_range(a:str, b:str, div: int):
         n += 1
         ending += 1
 
-    print(f"{starting=}, {ending=}")
 
     total = 0
     if n > 0:
@@ -82,23 +72,21 @@ def process_range(a:str, b:str, div: int):
             skipping=False
             for smaller in range(1, chunk_size):
                 if len(set(batched(str(i)*div, smaller))) == 1:
-                    print("skipping", str(i)*div)
                     skipping=True
                     break
             if skipping:
                 continue
-            print(str(i)*div)
             total += int(str(i) * div)
 
-
-    print(n)
-    print("!", total)
 
     return total
 
 from sys import stdin
 
+import time
+
 def process_all(inp):
+    start = time.perf_counter()
     for line in inp:
         total = 0
         ranges = line.split(",")
@@ -112,6 +100,8 @@ def process_all(inp):
 
         print("!!!")
         print(total)
+    total_time = time.perf_counter() - start
+    print(total_time)
 
 
 main_input=["61-71,12004923-12218173,907895-1086340,61083-74975,7676687127-7676868552,3328-4003,48-59,3826934-3859467,178-235,75491066-75643554,92-115,1487-1860,483139-586979,553489051-553589200,645895-722188,47720238-47818286,152157-192571,9797877401-9798014942,9326-11828,879837-904029,4347588-4499393,17-30,1-16,109218-145341,45794-60133,491-643,2155-2882,7576546102-7576769724,4104-5014,34-46,67594702-67751934,8541532888-8541668837,72-87,346340-480731,3358258808-3358456067,78265-98021,7969-9161,19293-27371,5143721-5316417,5641-7190,28793-36935,3232255123-3232366239,706-847,204915-242531,851-1135,790317-858666"]
